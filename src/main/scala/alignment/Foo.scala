1a6d21d3b200a0403aeae2fa67b08a30ff33ed23
David Kettlestrings <dkettlestrings@gmail.com>
2017-05-24 12:03:16 -0500

    Skeleton project

---
+++ src/main/scala/alignment/Foo.scala
@@ -0,0 +1,10 @@
+package alignment
+
+/**
+  * Created by dkettlestrings on 5/24/17.
+  */
+object Foo {
+
+  val bar = 7
+
+}
---
+++ src/test/scala/alignment/TestFoo.scala
@@ -0,0 +1,16 @@
+package alignment
+
+import org.scalatest.FunSuite
+
+/**
+  * Created by dkettlestrings on 5/24/17.
+  */
+class TestFoo extends FunSuite {
+
+  test("Foo") {
+
+    assert(Foo.bar == 7)
+
+  }
+
+}
